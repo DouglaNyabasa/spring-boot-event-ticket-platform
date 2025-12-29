0189b08289c83809777fa808fd2a4152fb785e71
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-29 17:08:35 +0200

    initial commit

---
+++ src/main/java/com/doug/eventtickentplatform/EventTickentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.doug.eventtickentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTickentPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTickentPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doug/eventtickentplatform/EventTickentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doug.eventtickentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTickentPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
