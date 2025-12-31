10c7ec822467010e9f754b5394300c5ce54483aa
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-31 19:10:02 +0200

    added user entity

---
+++ src/main/java/com/doug/eventticketplatform/model/User.java
@@ -0,0 +1,40 @@
+package com.doug.eventticketplatform.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @CreatedDate
+    @Column(name = "created_at",updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at",nullable = false)
+    private LocalDateTime updatedAt;
+
+
+
+}
