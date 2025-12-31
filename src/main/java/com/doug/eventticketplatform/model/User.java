63aee89a57763dbe1ae62360d2505398a59d65d0
douglanyabasa <douglasnyabasa400@gmail.com>
2025-12-31 19:34:49 +0200

    updated the user entity

--- src/main/java/com/doug/eventticketplatform/model/User.java
+++ src/main/java/com/doug/eventticketplatform/model/User.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -26,6 +28,22 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "user_attending_events",
+    joinColumns = @JoinColumn(name = "userid"),
+    inverseJoinColumns = @JoinColumn(name = "event_id"))
+    private List<Event> attendingEvents = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_staffing_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> staffingEvents = new ArrayList<>();
     @CreatedDate
     @Column(name = "created_at",updatable = false,nullable = false)
     private LocalDateTime createdAt;
