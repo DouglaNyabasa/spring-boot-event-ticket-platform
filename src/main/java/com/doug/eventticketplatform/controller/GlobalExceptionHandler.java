e86c62221d4e06d8b0ded27f7ec1edf053949b8b
douglanyabasa <douglasnyabasa400@gmail.com>
2026-01-05 20:04:28 +0200

    added global exception and custome exception

--- src/main/java/com/doug/eventticketplatform/controller/GlobalExceptionHandler.java
+++ src/main/java/com/doug/eventticketplatform/controller/GlobalExceptionHandler.java
@@ -1,16 +1,45 @@
 package com.doug.eventticketplatform.controller;
 
+import com.doug.eventticketplatform.Exceptions.UserNotFoundException;
 import com.doug.eventticketplatform.dto.ErrorDto;
+import jakarta.persistence.PreUpdate;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleUserNotFoundException(UserNotFoundException exception) {
+        log.error("User not exception", exception);
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setError("User Not Found");
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error("caught MethodArgumentNotValidException", exception);
+        ErrorDto errorDto = new ErrorDto();
+        BindingResult bindingResult = exception.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        String errorMessage = fieldErrors.stream().findFirst().map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage()).orElse(
+                "Validation Error Occurred"
+        );
+        errorDto.setError(errorMessage);
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorDto> handleConstraintViolation(ConstraintViolationException exception) {
