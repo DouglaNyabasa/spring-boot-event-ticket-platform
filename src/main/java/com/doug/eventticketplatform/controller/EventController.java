57357d6a4dab7ec0e3320a5b1d0f4b1cea8d6265
douglanyabasa <douglasnyabasa400@gmail.com>
2026-01-05 15:51:08 +0200

    added a create event method in controller

--- src/main/java/com/doug/eventticketplatform/controller/EventController.java
+++ src/main/java/com/doug/eventticketplatform/controller/EventController.java
@@ -1,4 +1,41 @@
 package com.doug.eventticketplatform.controller;
 
+import com.doug.eventticketplatform.dto.CreateEventRequestDto;
+import com.doug.eventticketplatform.dto.CreateEventResponseDto;
+import com.doug.eventticketplatform.mapper.EventMapper;
+import com.doug.eventticketplatform.model.Event;
+import com.doug.eventticketplatform.request.CreateEventRequest;
+import com.doug.eventticketplatform.service.EventService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "/api/v1/events")
 public class EventController {
+
+    private  final EventMapper eventMapper;
+    private final EventService eventService;
+
+    public EventController(EventMapper eventMapper, EventService eventService) {
+        this.eventMapper = eventMapper;
+        this.eventService = eventService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CreateEventResponseDto> createEvent(@AuthenticationPrincipal Jwt jwt, @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
 }
